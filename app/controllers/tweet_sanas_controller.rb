a6ef2e055ede1521e5bf3da81596c64d97d00094
Hedgeken95 <ken10951128@icloud.com>
2020-09-17 20:16:28 +0900

    ユーザー機能の実装

--- app/controllers/tweet_sanas_controller.rb
+++ app/controllers/tweet_sanas_controller.rb
@@ -1,5 +1,6 @@
 class TweetSanasController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = TweetSana.all
@@ -36,5 +37,11 @@ class TweetSanasController < ApplicationController
 
   def set_tweet
     @tweet_sana = TweetSana.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_023846) do
+ActiveRecord::Schema.define(version: 2020_09_17_081013) do
 
   create_table "tweet_sanas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,12 +20,17 @@ ActiveRecord::Schema.define(version: 2020_09_17_023846) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "tweetsanas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nickname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
